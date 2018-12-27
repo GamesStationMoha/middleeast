bedba5510ef60952cb583e03e4b598453ea66b99
GamesStationMoha <45317067+GamesStationMoha@users.noreply.github.com>
2018-12-27 23:45:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -645,6 +645,7 @@ client.on('message', async message => {
     },3000);
   });
   }
+
 });
 //temp
 const temp = {};
@@ -722,7 +723,46 @@ client.on('message', async message => {
            })
           }
       });
+//short
+const prefix = '+';
+var googl = require('goo.gl');
 
+googl.setKey('AIzaSyC9MdpZYw0ELyRQuAhz4ycYJnBUgE0BEDc');
+ 
+googl.getKey();
+ 
+client.on('ready', () => {
+    console.log('ready');
+}).on('message', message => {
+    let args = message.content.split(' ').slice(1);
+    if(message.content.startsWith(prefix + 'short')) {
+    googl.shorten(args[1])
+    .then(function (shortenUrl) {
+        message.channel.send(`الرابط المختصر: ${shortenUrl}`);
+    })
+    .catch(function (err) {
+        console.log(err.message);
+    });
+}
+});
+//welcome
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","users");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let norelden = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:norelden});          
+               
+ 
+      }
+      });
 //help
 
    client.on('message', message => {
