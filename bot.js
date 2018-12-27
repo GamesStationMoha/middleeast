58426d2f041f4993a313aa4d8529d773c4c4374a
GamesStationMoha <45317067+GamesStationMoha@users.noreply.github.com>
2018-12-27 02:32:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -129,7 +129,7 @@ client.on("message", message => {
 //mute
 client.on("message", message => {
   let command = message.content.split(" ")[0];
-  if (command === prefix + "mute" }  {
+  if (command === prefix + "mute")  {
           if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');
                   if (!message.member.hasPermission('MANAGE_ROLES')) return message.reply("** لا يوجد لديك برمشن 'Manage Roles' **");
   let user = message.mentions.users.first();
@@ -157,7 +157,7 @@ client.on("message", message => {
   }
 
 };
-    if (command === prefix + "unmute" }  {
+    if (command === prefix + "unmute")  {
           if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
         if (!message.member.hasPermission('MANAGE_ROLES')) return message.reply("** لا يوجد لديك برمشن 'Manage Roles' **");
   let user = message.mentions.users.first();
